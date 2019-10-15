5920f04f6c7ffe85a3ba6502ad0f1322794ff42a
Niels Van Hauwermeiren <niels.vanhauwermeiren@campus.romerocollege.be>
2019-10-15 10:42:14 +0200

    Add project files.

---
+++ Leeftijd/Program.cs
@@ -0,0 +1,23 @@
+﻿//5AIT3 - 2019-10-15 - Leeftijd
+/* OPGAVE:
+ * We vragen de gebruiker om diens geboortedatum en toon diens leeftijd (vereenvoudigde versie: enkel op basis van jaartal).
+ * 
+ * ANALYSE:
+ * ========
+ * 
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leeftijd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
